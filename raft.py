8c63443ff0119cadc967678b1775f50cea2ba49e
169kaif <kaifraushan@gmail.com>
2024-03-24 06:29:38 +0530

    add server client functionality for request vote, need to add more functionality to method, communication has been setup:

--- raft.py
+++ raft.py
@@ -3,10 +3,16 @@ import os
 import sys
 from random import randint
 import time
+import threading
+from concurrent import futures
+import grpc
+import raft_pb2
+import raft_pb2_grpc
 
 
 class Node:
-    def __init__(self, node_id):
+    def __init__(self, node_id, port, peer_addresses):
+        self.node_id = node_id
         self.current_term = 0
         self.voted_for = None
         self.log = []
@@ -17,10 +23,10 @@ class Node:
         self.sent_length = []
         self.acked_length = []
         self.database={}
+        self.peer_addresses = peer_addresses
 
         self.election_period_ms = randint(1000, 5000)
         self.rpc_period_ms = 3000
-        # self.election_timeout = -1
 
         node_log_location = f"node_id_{node_id}"
 
@@ -46,9 +52,27 @@ class Node:
         # else:
             ##do restoration
 
-        #start election timeout
-        self.election_timeout = time.monotonic() + self.election_period_ms
-        
+        #start server
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+        raft_pb2_grpc.add_ServicesServicer_to_server(server)
+        server.add_insecure_port(f'[::]:{port}')
+        server.start()
+        print(f"Node {self.node_id} listening on port {port}")
+        server.wait_for_termination()
+
+    #method to request votes from all peers
+    def request_vote(self):
+        for address in self.peer_addresses:
+            with grpc.insecure_channel(address) as channel:
+                stub = raft_pb2.ServicesStub(channel)
+
+                try:
+                    req_msg = raft_pb2.RequestVoteArgs(Term = str(self.current_term), CandidateID = str(self.node_id), LastLogIndex = str(len(self.log)-1), LastLogTerm = self.log[-1])
+                    response = stub.RequestVote(req_msg)
+                    print(f"Vote Request successfully sent to {address}")
+                except grpc.RpcError as e:
+                    print(f"Failed to send Vote Request to {address}")
+
 
 def nodeServe(Node):
     while True:
@@ -69,6 +93,10 @@ def nodeServe(Node):
 
         elif Node.current_role == "candidate":
 
+            #start election timer
+            election_start_time = time.monotonic()
+
+            #increment current term
             Node.current_term += 1
 
             #vote for self, need to, WRITE TO LOG AS WELL 
@@ -84,8 +112,16 @@ def nodeServe(Node):
                 Node.last_term = Node.log[-1]
 
             #request vote from all nodes inside a while loop
-            
-            
+            while (True):
+
+                #if election times out, send message again
+                if ((time.monotonic() - election_start_time)*1000 > Node.elections_period_ms):
+                    threading.Thread(target=Node.request_vote, daemon=True).start()
+
+
+                    #NEED TO WORK ON IF CONSENSUS RECEIVED, MOVE TO LEADER STATE
+                    #NEED TO MODIFY METHOD IN NODE CLASS TO JUDGE CONSENSUS
+                    #IF HIGHER TERM RECEIVED, STEP DOWN TO FOLLOWER STATE
 
 
 if __name__=="main":
